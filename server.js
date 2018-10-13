674b9493712f397920e2e5b0780dc422f616758d
Inna <designbyinna@gmail.com>
2018-10-13 13:39:54 -0600

    missing closure

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/reactnyt";
 // Set mongoose to leverage built in JavaScript ES6 Promises
 // Connect to the Mongo DB
 mongoose.Promise = Promise;
-mongoose.connect(MONGODB_URI
+mongoose.connect(MONGODB_URI)
 // Start the API server
 app.listen(PORT, function() {
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
